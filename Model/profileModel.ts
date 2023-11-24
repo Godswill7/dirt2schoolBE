475600b29417f23f7efd77db368ce4585d37c943
Godswill Udida <udidagodswill7@gmail.com>
2023-11-24 13:58:22 +0100

    godswill

---
+++ Model/profileModel.ts
@@ -0,0 +1,52 @@
+import { Schema, model, Types } from "mongoose";
+import { iProfileData } from "../utils/interface";
+
+const profileModel = new Schema(
+  {
+    address: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    balance: {
+      type: String,
+    },
+    avatar: {
+      type: String,
+    },
+    avatarID: {
+      type: String,
+    },
+    schoolName: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    motivation: {
+      type: String,
+      required: true,
+    },
+    bagHistory: [
+      {
+        type: Types.ObjectId,
+        ref: "bags",
+      },
+    ],
+    feeHistory: [
+      {
+        type: Types.ObjectId,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model<iProfileData>("profiles", profileModel);
--- utils/interface.ts
+++ utils/interface.ts
@@ -1,18 +1,38 @@
-import { Document } from "mongoose"
+import { Document } from "mongoose";
 
 export interface iAuth {
-    email:string
-    password:string
-    verify:boolean
-    token:string
-    secretKey:string
-    profile:{}[],
-}
-export interface iBag {
-    bag: number,
-    cash: number,
-    studentID: string,
+  email: string;
+  password: string;
+  verify: boolean;
+  token: string;
+  secretKey: string;
+  profile: {}[];
+  bagHistory: {}[];
+  feeHistory: {}[];
 }
 
-export interface iAuthData extends iAuth, Document{}
-export interface iBagData extends iBag, Document{}
+interface iProfile {
+  address: string;
+  PhoneNumber: number;
+  balance: number;
+  avatar: string;
+  avatarID: string;
+  schoolName: string;
+  gender: string;
+  motivation: string;
+}
+
+interface iFee {
+    ammountPaid: Number;
+}
+
+export interface iBag {
+  bag: number;
+  cash: number;
+  studentID: string;
+}
+
+export interface iAuthData extends iAuth, Document {}
+export interface iBagData extends iBag, Document {}
+export interface iProfileData extends iProfile, Document {}
+export interface iFeeData extends iFee, Document {}
