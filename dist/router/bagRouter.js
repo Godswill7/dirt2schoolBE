e8581ef1e6d13ab99d00744cd3713de62ab99e16
Godswill Udida <udidagodswill7@gmail.com>
2023-12-22 07:55:53 +0100

    dist

--- dist/router/bagRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const bagController_1 = require("../Controller/bagController");
-const router = (0, express_1.Router)();
-router.route("/:studentID/create-bag").post(bagController_1.createBag);
-exports.default = router;
--- dist/router/feeRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const feeController_1 = require("../Controller/feeController");
-const router = (0, express_1.Router)();
-router.route("/:schoolID/:studentID/create-fee").post(feeController_1.createFee);
-exports.default = router;
